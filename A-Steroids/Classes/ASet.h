b7957faccf615716289dd280db8e622544effe96
mumreg <mumreg@gmail.com>
2016-02-17 12:19:08 +0300

    Update ASet.h

--- A-Steroids/Classes/ASet.h
+++ A-Steroids/Classes/ASet.h
@@ -5,6 +5,7 @@
 //  Created by Mikhail Perekhodtsev on 01.07.13.
 //  Copyright (c) 2013 Mikhail Perekhodtsev. All rights reserved.
 //
+//  This is test commit
 
 #ifndef __A_Steroids__ASet__
 #define __A_Steroids__ASet__
