324f789f975ea2ae0d3d13bcd2cc705905a8588f
Mikhail Perekhodtsev <m.perekhodtsev@rocketjump.ru>
2016-02-17 16:16:48 +0300

    Test commit

--- A-Steroids/Classes/ASet.h
+++ A-Steroids/Classes/ASet.h
@@ -5,7 +5,6 @@
 //  Created by Mikhail Perekhodtsev on 01.07.13.
 //  Copyright (c) 2013 Mikhail Perekhodtsev. All rights reserved.
 //
-//  This is test commit
 
 #ifndef __A_Steroids__ASet__
 #define __A_Steroids__ASet__
